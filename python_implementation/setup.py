267c32856d40bf7502cf91f3c11eba47ee6e93ab
CrepeGoat <awqatty.b@gmail.com>
2019-09-14 12:40:49 -0400

    initial commit for python implementation

---
+++ python_implementation/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    # Metadata
+    name='CFQ',
+    version='0.0.1',
+    description='A package for storing rational numbers in continued-fraction'
+                ' format',
+    author='CrepeGoat',
+    # Contents
+    packages=['cfq'],
+    # Dependencies
+    #install_requires=['sortedcontainers'],
+    #tests_requires=['pytest'],
+)
