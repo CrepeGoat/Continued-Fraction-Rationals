729d8a66128547b87a13a059086178bdbcf68c0e
CrepeGoat <awqatty.b@gmail.com>
2019-09-15 19:04:50 -0400

    added basic static-sequence cfq class
    - simply stores values in python list -> TODO optimize

---
+++ python_implementation/cfq/statics.py
@@ -0,0 +1,90 @@
+import abc
+import fractions
+import numbers
+
+
+class CFRationalBase(abc.ABC):
+    @abc.abstractmethod
+    def __intiter__(self):
+        pass
+
+
+class IntSequenceBase(abc.ABC):
+    @abc.abstractmethod
+    def __next__(self):
+        pass
+
+
+###############################################################################
+#
+###############################################################################
+
+class StaticCFRational(CFRationalBase):#, numbers.Real):
+    def __init__(self, sequence):
+        self._sequence = sequence
+
+    # -------------------------------------------------------------------------
+    # Core Methods
+    # -------------------------------------------------------------------------
+
+    def __intiter__(self):
+        return ((i, True) for i in self._sequence)
+
+    # -------------------------------------------------------------------------
+    # Conversions in
+    # -------------------------------------------------------------------------
+
+    @classmethod
+    def from_fraction(cls, x):
+        sequence = []
+
+        while True:
+            x_int = int(x)
+            sequence.append(x_int)
+
+            if x == x_int:
+                break
+            x = 1/(x-x_int)
+
+        return StaticCFRational(sequence)
+
+    @classmethod
+    def from_float(cls, x):
+        return cls.from_fraction(fractions.Fraction(x))
+
+    @classmethod
+    def from_int(cls, x):
+        return StaticCFRational([x])
+
+    # -------------------------------------------------------------------------
+    # Conversions out
+    # -------------------------------------------------------------------------
+
+    def to_fraction(self):
+        val = fractions.Fraction(1)
+        val._denominator = 0
+
+        for i in self._sequence[::-1]:
+            val = i + 1/val
+        return val
+
+    def __float__(self):
+        return float(self.to_fraction())
+
+    def __int__(self):
+        return int(self.to_fraction())
+
+
+###############################################################################
+#
+###############################################################################
+
+def rational(x):
+    if isinstance(x, int):
+        return StaticCFRational.from_int(x)
+    if isinstance(x, float):
+        return StaticCFRational.from_float(x)
+    if isinstance(x, fractions.Fraction):
+        return StaticCFRational.from_fraction(x)
+
+    return StaticCFRational.from_float(float(x))
